88e025977115e264225740db9697278beb49786b
saurabh <saurabhverma2693@gmail.com>
2026-07-18 13:29:11 +0530

    Work with Fragments

--- src/App.jsx
+++ src/App.jsx
@@ -28,7 +28,7 @@ function App() {
   }
 
   return (
-    <div>
+    <>
       <Header />
       <main>
         <section id="core-concepts">
@@ -52,7 +52,7 @@ function App() {
           {tabContent}
         </section>
       </main>
-    </div>
+    </>
   );
 }
 
